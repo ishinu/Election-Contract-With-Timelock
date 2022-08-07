0cd570a43c51552ebf9f1e601138f1e96c8abd36
ishinu <tysmgroups@gmail.com>
2022-08-07 17:37:50 +0530

    2. Unit testing ( Chai.js )

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -24,20 +24,23 @@ contract Election {
 
 */
 
-  uint256 public votingTime = 1659851092;  // Add 1 minute in it.
-  string public winner;
+  uint256 public votingTime = 1659874000;  // Add 1 minute in it.
+  // string public winner;
   uint public id;
   struct Candidate{
     uint id;
     string name;
     uint votes;
   }
-  let candidates = [];
+  uint[] public loop;
+  
+  // let candidates = [];
   // uint[] public candidates;
-  // mapping(uint=>Candidate) public candidates;
+  mapping(uint=>Candidate) public candidates;
   // let candidates = {};
   mapping(address=>bool) public voted;
 
+  //tested
   modifier alreadyVoted{
     require(!voted[msg.sender]);           // Check if msg.sender already voted.
     require(block.timestamp < votingTime); // Check if voting duration already finished.
@@ -45,45 +48,73 @@ contract Election {
   }
 
   modifier timeIsUp{
-    require(block.timestamp> votingTime);
+    require(block.timestamp > votingTime);
     _;
   }
 
-  // constructor(){
-  //   increment();
-  //   candidates[id] = Candidate(id,"First",0);
-  //   increment();
-  //   candidates[id] = Candidate(id,"Second",0);
-  // }
-
-  // function addCandidate(string memory _name) public{
-  //   increment();
-  //   candidates[id] = Candidate(id,_name,0);
-  // }
-
-  // function increment() public{
-  //   id += 1;
-  // }
-
-  // function vote(uint _choice) public alreadyVoted{
-  //   voted[msg.sender] = true;
-  //   for(uint i=1;i<candidates.length;i++){
-  //     if(i==_choice){
-  //       candidates[i].votes += 1;
-  //     }
-  //   }
-  // }
-
-  // function assignWinner() external timeIsUp{
-  //   for(uint i =1;i<candidates.length;i++){
-  //     if(candidates[i].votes>candidates[i+1].votes){
-  //       winner = candidates[i].name;
-  //     }
-  //   }
-  // }
-
-  function checkWinner() view public returns(string memory) {
-    return winner;
+  //tested
+  constructor(){
+    increment();
+    candidates[id] = Candidate(id,"First",0);
+    increment();
+    candidates[id] = Candidate(id,"Second",0);
   }
+
+  //tested
+  function addCandidate(string memory _name) public{
+    increment();
+    candidates[id] = Candidate(id,_name,0);
+  }
+
+  //tested
+  function increment() public{
+    id += 1;
+    loop.push(id);
+  }
+
+  //tested
+  function vote(uint _choice) public alreadyVoted{
+    voted[msg.sender] = true;
+    // for(uint i=1;i<candidates.length;i++){
+    //   if(i==_choice){
+    //     candidates[i].votes += 1;
+    //   }
+    // }
+    for( uint i = 0; i<loop.length; i++ ){
+      if (loop[i] == _choice){
+        candidates[loop[i]].votes += 1;
+        break;
+      }
+    }
+  }
+  //tested
+  function checkVotes(uint _id) view public returns (uint _num){
+    for( uint i = 0; i < loop.length; i++){
+      if(loop[i]== _id){
+        _num = candidates[loop[i]].votes;
+        break;
+      }
+    }
+    return _num;
+  }
+  //tested
+  function assignWinner() view public returns (string memory _winner){
+    uint g = candidates[loop[0]].votes;
+    for( uint y = 0; y < loop.length; y++){
+      if(g < candidates[loop[y]].votes){
+        g = candidates[loop[y]].votes;
+      }
+    } 
+    for( uint y = 0 ; y < loop.length; y++){
+      if(candidates[loop[y]].votes == g){
+        _winner = candidates[loop[y]].name;
+      }
+    }
+    return _winner;
+  }
+  //tested
+  // function checkWinner() view public returns(string memory) {
+  //   return winner;
+  // }
   
 }
